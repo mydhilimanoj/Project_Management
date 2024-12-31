80256ac74ecde00998d27857f5cfffb465256fe2
mydhilimanoj <mydhilimydhus3@gmail.com>
2024-12-31 13:49:13 +0530

    Initial commit

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, ProjectViewSet, TaskViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'projects', ProjectViewSet)
+router.register(r'tasks', TaskViewSet)
+router.register(r'comments', CommentViewSet)
+
+urlpatterns = router.urls
---
+++ api/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from .models import User, Project, Task, Comment
+from .serializers import UserSerializer, ProjectSerializer, TaskSerializer, CommentSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
